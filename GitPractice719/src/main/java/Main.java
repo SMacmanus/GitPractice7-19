d3fd5225aad36c83665192471e6fe8411378c5c8
SMacmanus <brinamac24@gmail.com>
2019-07-19 09:45:50 -0500

    commit concat branch

--- GitPractice719/src/main/java/Main.java
+++ GitPractice719/src/main/java/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+    String a = "Hello ";
+    String b = "world ";
+    String c = "and ";
+    String d = "goodnight.";
+
     public static void main(String[] args){
         System.out.println("Hello World");
+    }
+
+    public void concatOne(String a, String b, String c, String d){
+        System.out.println(a + b + c + d);
+    }
+    public void concatOne(String a, String b){
+        System.out.println(a + b);
     }
 }

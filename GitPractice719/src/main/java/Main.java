098a0e3f78986e04351e322657033380482fecda
SMacmanus <brinamac24@gmail.com>
2019-07-19 09:37:23 -0500

    commit math branch

--- GitPractice719/src/main/java/Main.java
+++ GitPractice719/src/main/java/Main.java
@@ -1,5 +1,31 @@
 public class Main {
+    double a;
+    double b;
+    double c;
+
     public static void main(String[] args){
         System.out.println("Hello World");
+    }
+
+    public void add(double a, double b){
+        System.out.println(a + b);
+    }
+    public void subtract(double a, double b){
+        System.out.println(a - b);
+    }
+    public void multiply(double a, double b){
+        System.out.println(a * b);
+    }
+    public void divide(double a, double b){
+        System.out.println(a / b);
+    }
+    public void exponentiate(double a, double b) {
+        System.out.println(Math.pow( a, b));
+    }
+    public void add(double a, double b, double c){
+        System.out.println(a + b + c);
+    }
+    public void multiply(double a, double b, double c){
+        System.out.println(a * b * c);
     }
 }
